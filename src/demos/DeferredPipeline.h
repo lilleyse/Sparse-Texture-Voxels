1bf25dcfbe8de95dfde435429d0abe45c47d561d
Sean Lilley <seanlilley@ymail.com>
2012-09-30 17:23:19 -0400

    removed tabs in deferredPipeline.h

--- src/demos/DeferredPipeline.h
+++ src/demos/DeferredPipeline.h
@@ -26,45 +26,45 @@ public:
     {
         // Creation positions texture
         glGenTextures(1, &positionsTexture);
-	    glBindTexture(GL_TEXTURE_2D, positionsTexture);
+        glBindTexture(GL_TEXTURE_2D, positionsTexture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // Create colors texture
         glGenTextures(1, &colorsTexture);
-	    glBindTexture(GL_TEXTURE_2D, colorsTexture);
+        glBindTexture(GL_TEXTURE_2D, colorsTexture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, screenWidth, screenHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // Create normals texture
         glGenTextures(1, &normalsTexture);
-	    glBindTexture(GL_TEXTURE_2D, normalsTexture);
+        glBindTexture(GL_TEXTURE_2D, normalsTexture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, screenWidth, screenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // Create depth texture
         glGenTextures(1, &depthTexture);
-	    glBindTexture(GL_TEXTURE_2D, depthTexture);
+        glBindTexture(GL_TEXTURE_2D, depthTexture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, screenWidth, screenHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 
-	    // Create deffered framebuffer object
-	    glGenFramebuffers(1, &deferredFBO);
-	    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, deferredFBO);
-	    glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture, 0);
+        // Create deffered framebuffer object
+        glGenFramebuffers(1, &deferredFBO);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, deferredFBO);
+        glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture, 0);
         glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, positionsTexture, 0);  
         glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, colorsTexture, 0); 
         glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, normalsTexture, 0);
         uint fboAttachments[3] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2};
         glDrawBuffers(3, fboAttachments);
 
-	    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-	    glBindTexture(GL_TEXTURE_2D, 0);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        glBindTexture(GL_TEXTURE_2D, 0);
 
         // Bind all the textures so we can read back from them later in a separate shader
         glActiveTexture(GL_TEXTURE0 + DEFERRED_POSITIONS_BINDING);
@@ -128,15 +128,15 @@ public:
         
         // Clear depth textures
         float clearDepth = 1.0f;
-		glClearBufferfv(GL_DEPTH, 0, &clearDepth);
+        glClearBufferfv(GL_DEPTH, 0, &clearDepth);
        
         // Clear color attachment textures (positions, colors, normals)
-	    float clearColor[4] = {0.0f,0.0f,0.0f,0.0f};
+        float clearColor[4] = {0.0f,0.0f,0.0f,0.0f};
         for(int i = 0; i < numColorAttachments; i++)
         {
             glClearBufferfv(GL_COLOR, i, clearColor);
         }
-		
+        
         // Write to the FBO
         debugDraw.display(deferredWriteProgram);
 
